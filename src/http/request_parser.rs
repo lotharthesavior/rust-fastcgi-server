1f5ecd462f3d22847e5eb7fc9780da658829e4ea
Savio Resende <savio@savioresende.com>
2024-10-13 17:26:18 -0500

    Added some tests.

--- src/http/request_parser.rs
+++ src/http/request_parser.rs
@@ -8,3 +8,44 @@ pub fn parse_http_request(request: &str) -> (&str, &str, &str) {
     let version = parts.next().unwrap_or("");
     (method, path, version)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_http_request() {
+        let request = "GET /index.html HTTP/1.1\r\nHost: example.com\r\n";
+        let (method, path, version) = parse_http_request(request);
+        assert_eq!(method, "GET");
+        assert_eq!(path, "/index.html");
+        assert_eq!(version, "HTTP/1.1");
+    }
+
+    #[test]
+    fn test_incomplete_http_request() {
+        let request = "POST /submit";
+        let (method, path, version) = parse_http_request(request);
+        assert_eq!(method, "POST");
+        assert_eq!(path, "/submit");
+        assert_eq!(version, "");
+    }
+
+    #[test]
+    fn test_empty_request() {
+        let request = "";
+        let (method, path, version) = parse_http_request(request);
+        assert_eq!(method, "");
+        assert_eq!(path, "");
+        assert_eq!(version, "");
+    }
+
+    #[test]
+    fn test_malformed_request_line() {
+        let request = "INVALID\r\nSome-Header: value";
+        let (method, path, version) = parse_http_request(request);
+        assert_eq!(method, "INVALID");
+        assert_eq!(path, "");
+        assert_eq!(version, "");
+    }
+}
--- src/http/static_file.rs
+++ src/http/static_file.rs
@@ -1,14 +1,14 @@
 use mime_guess::from_path;
-use tokio::fs;
+use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub fn is_allowed_static_file(file_path: &str) -> bool {
-    let allowed_extensions = vec![".css", ".js", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico"];
+    let allowed_extensions = vec![".css", ".js", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico", ".html"];
     allowed_extensions.iter().any(|ext| file_path.ends_with(ext))
 }
 
 pub async fn serve_static_file(file_path: &str, socket: &mut tokio::net::TcpStream) {
-    if let Ok(mut file) = fs::File::open(file_path).await {
+    if let Ok(mut file) = File::open(file_path).await {
         let mut file_contents = Vec::new();
         file.read_to_end(&mut file_contents).await.unwrap();
 
@@ -26,5 +26,28 @@ pub async fn serve_static_file(file_path: &str, socket: &mut tokio::net::TcpStre
     } else {
         let response = "HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n".to_string();
         socket.write_all(response.as_bytes()).await.unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::http::static_file::is_allowed_static_file;
+
+    #[test]
+    fn test_valid_static_files() {
+        assert_eq!(is_allowed_static_file("test.css"), true);
+        assert_eq!(is_allowed_static_file("test.js"), true);
+        assert_eq!(is_allowed_static_file("index.png"), true);
+        assert_eq!(is_allowed_static_file("index.jpg"), true);
+        assert_eq!(is_allowed_static_file("index.jpeg"), true);
+        assert_eq!(is_allowed_static_file("index.gif"), true);
+        assert_eq!(is_allowed_static_file("index.svg"), true);
+        assert_eq!(is_allowed_static_file("index.ico"), true);
+        assert_eq!(is_allowed_static_file("index.html"), true);
+    }
+
+    #[test]
+    fn test_not_accepted_as_static_file() {
+        assert_eq!(is_allowed_static_file("index.php"), false);
     }
 }
